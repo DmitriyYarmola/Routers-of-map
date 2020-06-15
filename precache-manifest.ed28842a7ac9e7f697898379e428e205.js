0528973ce62c81b87d2b3c2b662e338d9b11fbf8
Dmitriy <dmitriy.yarmola@gmail.com>
2020-06-15 14:31:03 +0300

    Updates

---
+++ precache-manifest.ed28842a7ac9e7f697898379e428e205.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9a94e9bfff80c61cee7d7d134da4fa1",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "aa888a0f3d265dda650d",
+    "url": "/react-gh-pages/static/css/2.4446f115.chunk.css"
+  },
+  {
+    "revision": "ce00fa6778926be0d68b",
+    "url": "/react-gh-pages/static/css/main.a4b18de8.chunk.css"
+  },
+  {
+    "revision": "aa888a0f3d265dda650d",
+    "url": "/react-gh-pages/static/js/2.f41ba4c8.chunk.js"
+  },
+  {
+    "revision": "876e403427d7be82fb4e8ac24123aaf3",
+    "url": "/react-gh-pages/static/js/2.f41ba4c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce00fa6778926be0d68b",
+    "url": "/react-gh-pages/static/js/main.2b71c5ae.chunk.js"
+  },
+  {
+    "revision": "6ff939d621f4b1c08e3f",
+    "url": "/react-gh-pages/static/js/runtime-main.39256a51.js"
+  },
+  {
+    "revision": "e3380abb934aa9b037febf89edcc5b93",
+    "url": "/react-gh-pages/static/media/cross.e3380abb.svg"
+  }
+]);
